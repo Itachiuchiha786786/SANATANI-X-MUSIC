6d9a736b1bcc5822f18b8187ff167ee9aa2d6d70
Itachiuchiha786786 <166844972+Itachiuchiha786786@users.noreply.github.com>
2024-09-28 18:51:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -147,7 +147,7 @@ PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://files.catbox.moe/xwms95.jpg"
 )
 PLAYLIST_IMG_URL = "https://telegra.ph/file/d723f4c80da157fca1678.jpg"
-STATS_IMG_URL = "https://telegra.ph/file/d30d11c4365c025c25e3e.jpg"
+STATS_IMG_URL = "https://files.catbox.moe/xwms95.jpg"
 TELEGRAM_AUDIO_URL = "https://telegra.ph/file/b8c7d332ca2951938a663.jpg"
 TELEGRAM_VIDEO_URL = "https://telegra.ph/file/e575ae40d6635250974e1.jpg"
 STREAM_IMG_URL = "https://telegra.ph/file/03efec694e41e891b29dc.jpg"
